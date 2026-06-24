4fdae99406f39afa3bfe3b919597ea895e28a8a0
andrea-dellanno <andrea.dellanno001@studenti.uniparthenope.it>
2026-06-24 16:44:21 +0200

    Delete mensa directory

--- mensa/templatetags/mensa_extras.py
+++
@@ -1,7 +0,0 @@
-from django import template
-register = template.Library()
-
-@register.filter
-def getattribute(obj, attr):
-    value = getattr(obj, attr, '')
-    return value() if callable(value) else value
